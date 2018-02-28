1d3d0799a0a933a89c8e34635c70f394d4b234ed
GiacomoOracolo <gzano93@gmail.com>
2018-02-28 18:09:16 +0100

    new crypt lib

--- dbhandlermodules/userauthentication.js
+++ dbhandlermodules/userauthentication.js
@@ -1,14 +1,13 @@
 var dbConnection = require('./databaseconnection')
 var mongoose = require('mongoose')
 var Schema = mongoose.Schema
-var bcrypt = require('bcrypt')
-
+var password = require('password-hash-and-salt');
 const saltRounds = 10;
 
 var UserSchema = new Schema({
-    username:{
-        type:String,
-        unique:true
+    username: {
+        type: String,
+        unique: true
     },
     email: {
         type: String,
@@ -23,35 +22,45 @@ var User = mongoose.model('User', UserSchema)
 
 
 
-exports.userSignUp = function (userName,userEmail, userPassword, socket) {
+exports.userSignUp = function (userName, userEmail, userPassword, socket) {
     socket.emit('signing up')
-    bcrypt.hash(userPassword, saltRounds, function (err, hash) {
-        var user = new User({ username:userName,email: userEmail, password: hash })
-        console.log("i'm signing up! " + user.email + " " + user.password)
-        user.save(function (err,user) {
-            console.log(err)
-            if(err){
-                console.log('error '+err)
-                socket.emit('error save',err.code)
-            }else{  
-                console.log('correctly saved user '+user)
-                socket.emit('user saved')
-            }
-        })
+    password(userPassword).hash(function (err, hash) {
+        if (err) {
+            console.log('errore sign up '+err)
+        }
+        else {
+            var user = new User({ username: userName, email: userEmail, password: hash })
+            console.log("i'm signing up! " + user.email + " " + user.password)
+            user.save(function (err, user) {
+                console.log(err)
+                if (err) {
+                    console.log('error ' + err)
+                    socket.emit('error save', err.code)
+                } else {
+                    console.log('correctly saved user ' + user)
+                    socket.emit('user saved')
+                }
+            })
+        }
+
     })
 
 
 }
-exports.userLogIn = function (userEmail,userPassword,socket) {
-    User.findOne({'email':userEmail},function(error,result){
-        console.log(result)
-        if(error) return new Error('errore query')
-        else{
-            bcrypt.compare(userPassword,result.password,function(error,same){
-                if(same){
-                    socket.emit('user loggedin',result)
+exports.userLogIn = function (userEmail, userPassword, socket) {
+    User.findOne({ 'email': userEmail }, function (error, result) {
+        if (error) return new Error('errore query')
+        else {
+            password(userPassword).verifyAgainst(result.password, function (error, same) {
+               if(error){
+                   console.log(error)
+               }
+                if (same) {
+                    console.log('user correct')
+                    socket.emit('user loggedin', result)
                 }
-                else{
+                else {
+                    
                     throw new Error('errore identità')
                 }
             })
