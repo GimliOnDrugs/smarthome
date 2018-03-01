e86592e31c21cdc27ab44ee98222ce7b10fcfb7c
GiacomoOracolo <gzano93@gmail.com>
2018-03-01 20:39:56 +0100

    minor

--- public/js/servicehandler.js
+++ public/js/servicehandler.js
@@ -1,16 +1,16 @@
 $(document).ready(function () {
     var user = JSON.parse(sessionStorage.getItem('currentuser'))
-    var socket=io('/'+user.username)
+    var socket = io('/' + user.username)
     console.log(user.username + " " + user.email)
     $('#userdisplayname').text("Welcome " + user.username)
-    
-   
+    var user=0
+    socket.on('update connected clients', function () {
+        user++
+        console.log(user)
+    })
+
 
 })
-var socket=io('/'+user.username)
-var user
-socket.on('update connected clients', function(){
-    user++
-    console.log(user)
-})
+/* var socket=io('/'+user.username)
+ */
 
