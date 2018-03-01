7ad8b3a0d137714f42902c1ee2338183f50e49cb
GiacomoOracolo <gzano93@gmail.com>
2018-03-01 21:42:33 +0100

    minor

--- public/js/servicehandler.js
+++ public/js/servicehandler.js
@@ -1,12 +1,13 @@
+var user=0
+
 $(document).ready(function () {
     var user = JSON.parse(sessionStorage.getItem('currentuser'))
     var socket = io('/' + user.username)
     console.log(user.username + " " + user.email)
     $('#userdisplayname').text("Welcome " + user.username)
-    var user=0
     socket.on('handshake', function () {
         user++
-        console.log(user)
+        console.log(user+" socket id: "+socket.id)
     })
 
 
