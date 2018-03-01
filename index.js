9b7d1f0f79395d62468bafee7004d4d64e9e9559
GiacomoOracolo <gzano93@gmail.com>
2018-03-01 19:12:54 +0100

    master

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
 var path = require('path');
 var server = require('http').createServer(app);
-var io = require('socket.io')(server, { wsEngine: 'ws' });
+var io = require('socket.io')(server);
 var port = process.env.PORT || 3000;
 var userauth = require('./dbhandlermodules/userauthentication')
 
