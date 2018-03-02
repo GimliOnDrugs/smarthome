481f456d6a3b7e3afce712c4d19a0cb8f0d56281
GiacomoOracolo <gzano93@gmail.com>
2018-03-02 14:27:22 +0100

    client and raspberry open a comunication channel in room

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ var userauth = require('./dbhandlermodules/userauthentication')
 app.use(express.static(path.join(__dirname, 'public')));
 
 
+
 io.on('connection', function (socket) {
   console.log('user connected ' + socket.id)
   socket.on('sign up', function (data) {
@@ -23,32 +24,32 @@ io.on('connection', function (socket) {
   })
 
   socket.on('room', (data) => {
-    /* userIo = io.of('/' + data.username)
-    userIo.on('connection', function (socket) {
-      console.log('connected to nsp james' + socket.id)
-      socket.emit('handshake', data.username + " connected users: " + users)
-
-      socket.on('disconnect', function (socket) {
-        console.log('socket disconnected: ' + socket.id)
-      })
-    })
- */
 
 
-    console.log(data + " is joining a room")
+    console.log(socket.id + " is joining room "+data)
 
     socket.join(data)
     io.in(data).clients(function (error, clients) {
       if (error) throw error
       console.log(clients)
     })
-    io.to(data).emit('room joined', { message: 'ciao!' })
+    socket.broadcast.to(data).emit('room joined', {roomjoined:data, id: socket.id })
+  })
+
+
+
+  socket.on('connect rpi', (data) => {
+
+    console.log(data)
+
+    socket.broadcast.emit('rpi',data)
+
 
   })
 
 
   socket.on('disconnect', function () {
-    console.log('user disconnected ')
+    console.log('user disconnected '+socket.id)
   })
 });
 
--- public/js/servicehandler.js
+++ public/js/servicehandler.js
@@ -1,20 +1,25 @@
+var socket = io()
+var user = JSON.parse(sessionStorage.getItem('currentuser'))
+
 
 $(document).ready(function () {
-    var user = JSON.parse(sessionStorage.getItem('currentuser'))
-    var socket = io()
-    socket.on('ciao',function(data){
-        console.log(data)
-    })
+
     console.log(user.username + " " + user.email)
     $('#userdisplayname').text("Welcome " + user.username)
     console.log(user.username)
-    socket.emit('room',user.username)
-    socket.on('room joined',function(data){
-        console.log('room joined '+data.message+" socket id "+socket.id)
+    socket.emit('room', user.username)
+    socket.on('room joined', function (data) {
+        console.log('room joined ' + data.message + " by " + data.id)
+
     })
 
 
+
 })
+function onRegisterClick() {
+    console.log('click')
+    socket.emit('connect rpi', { ipaddress: '192.168.1.185', name: user.username })
+}
 /* var socket=io('/'+user.username)
  */
 
