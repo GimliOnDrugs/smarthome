a26314cb92c3b81bd1f9aeabc7178b6c866b629d
GiacomoOracolo <gzano93@gmail.com>
2018-03-18 18:31:43 +0100

    minro

--- camerascript.py
+++ camerascript.py
@@ -7,9 +7,12 @@ from picamera import PiCamera
 message = sys.stdin.readline()
 
 if message == 'take pic':
-camera = PiCamera()
-camera.resolution = (1024, 768)
-camera.start_preview()
-# Camera warm-up time
-sleep(2)
-camera.capture('foo.jpg')
+ camera = PiCamera() 
+ camera.resolution = (1024, 768)
+ camera.start_preview()
+# Camera warm-up time 
+ sleep(2)
+ camera.capture('foo.jpg')
+else:
+
+	print('nope')
