5e76dfeb129dc78c9fa5fcba2a7ef92979fbef05
GiacomoOracolo <gzano93@gmail.com>
2018-03-18 18:55:15 +0100

    indentation error fixed on py script

--- camerascript.py
+++ camerascript.py
@@ -7,13 +7,13 @@ from picamera import PiCamera
 message = sys.stdin.readline()
 
 if message == 'take pic':
-    print(message=='take pic', ' ', message)
- camera = PiCamera() 
- camera.resolution = (1024, 768)    
- camera.start_preview()
-# Camera warm-up time 
- sleep(2)
- camera.capture('foo.jpg')
+    print(message == 'take pic', ' ', message)
+    camera = PiCamera()
+    camera.resolution = (1024, 768)
+    camera.start_preview()
+# Camera warm-up time
+    sleep(2)
+    camera.capture('foo.jpg')
 else:
 
 	print('nope')
