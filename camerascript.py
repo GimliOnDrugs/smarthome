25ff909e8e0ef1fb1c29c420c028133692d40133
GiacomoOracolo <gzano93@gmail.com>
2018-03-20 21:05:03 +0100

    again indentation

--- camerascript.py
+++ camerascript.py
@@ -5,21 +5,28 @@ import picamera
 import random
 
 message = sys.stdin.readline()
+
+
 def motion_detected():
     # Randomly return True (like a fake motion detection routine)
     return random.randint(0, 10) == 0
 
+camera = picamera.PiCamera()
+stream = picamera.PiCameraCircularIO(camera, seconds=20)
+camera.start_recording(stream, format='h264')
+
 if message == "take pic\n":
+
         try:
-           while True:
-            camera.wait_recording(1)
-            if motion_detected():
-            # Keep recording for 10 seconds and only then write the
-            # stream to disk
-                  camera.wait_recording(10)
-                  stream.copy_to('motion.h264')
+            while True:
+                camera.wait_recording(1)
+                if motion_detected():
+                    # Keep recording for 10 seconds and only then write the
+                    # stream to disk
+                    camera.wait_recording(10)
+                    stream.copy_to('motion.h264')
         finally:
             camera.stop_recording()
-    
+
 else:
     print('ghey')
