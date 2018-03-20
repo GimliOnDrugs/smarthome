f585e65a58691c12e53c87f777679537c80cd190
GiacomoOracolo <gzano93@gmail.com>
2018-03-20 20:19:16 +0100

    hudiojoopk

--- camerascript.py
+++ camerascript.py
@@ -7,16 +7,6 @@ import picamera
 message = sys.stdin.readline()
 
 if message == "take pic\n":
-    camera = picamera.PiCamera()
-    stream = picamera.PiCameraCircularIO(camera, seconds=20)
-    camera.start_recording(stream, format='h264')
-    print('recording started')
-
-    try:
-       while True:
-        stream.copy_to('motion.h264')
-    finally:
-       camera.stop_recording()
-       print('pic taken')
+    print('frocio')
 else:
     print('ghey')
