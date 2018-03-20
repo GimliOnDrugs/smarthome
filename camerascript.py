19a7a0e61a27c62d343eb4db3252abe59c18c3f9
GiacomoOracolo <gzano93@gmail.com>
2018-03-20 20:09:56 +0100

    minor

--- camerascript.py
+++ camerascript.py
@@ -5,21 +5,16 @@ from time import sleep
 import picamera
 
 message = sys.stdin.readline()
-def motion_detected():
-    # Randomly return True (like a fake motion detection routine)
-    return random.randint(0, 10) == 0
-""" if message == "take pic\n": """
-camera = picamera.PiCamera()
-stream = picamera.PiCameraCircularIO(camera, seconds=20)
-camera.start_recording(stream, format='h264')
-try:
-      while True:
-        camera.wait_recording(1)
-        if motion_detected():
-            # Keep recording for 10 seconds and only then write the
-            # stream to disk
-            camera.wait_recording(10)
-            stream.copy_to('motion.h264')
-finally:
-        camera.stop_recording()
-        print('pic taken')
+
+if message == "take pic\n":
+    camera = picamera.PiCamera()
+    stream = picamera.PiCameraCircularIO(camera, seconds=20)
+    camera.start_recording(stream, format='h264')
+    print('recording started')
+
+    try:
+       while True:
+        stream.copy_to('motion.h264')
+    finally:
+       camera.stop_recording()
+       print('pic taken')
