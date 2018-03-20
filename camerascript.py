11fda8a68f50e1da718152ecc5676e599c26ea12
GiacomoOracolo <gzano93@gmail.com>
2018-03-20 19:49:37 +0100

    minor

--- camerascript.py
+++ camerascript.py
@@ -10,11 +10,14 @@ if message == "take pic\n":
     camera = picamera.PiCamera()
     stream = picamera.PiCameraCircularIO(camera, seconds=20)
     camera.start_recording(stream, format='h264')
-    """ print('recording started') """
-
     try:
-       while True:
-        stream.copy_to('motion.h264')
+      while True:
+        camera.wait_recording(1)
+        if motion_detected():
+            # Keep recording for 10 seconds and only then write the
+            # stream to disk
+            camera.wait_recording(10)
+            stream.copy_to('motion.h264')
     finally:
-       camera.stop_recording()
-       print('pic taken')
+        camera.stop_recording()
+        print('pic taken')
