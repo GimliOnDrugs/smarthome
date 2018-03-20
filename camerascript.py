598083b2ba8f01286596fdfac6cc6ffb051c0cad
GiacomoOracolo <gzano93@gmail.com>
2018-03-20 20:49:31 +0100

    ijfeoidops

--- camerascript.py
+++ camerascript.py
@@ -7,12 +7,13 @@ import picamera
 message = sys.stdin.readline()
 
 if message == "take pic\n":
-        
+
     camera = picamera.PiCamera()
     camera.resolution = (640, 480)
     camera.start_recording('my_video.h264')
-    print('frocio')
+
     camera.wait_recording(60)
     camera.stop_recording()
+        print('frocio')
 else:
     print('ghey')
