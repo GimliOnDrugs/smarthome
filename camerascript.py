a96d7d294eb9852a56a8d141ae2fb612d5e2cbaf
GiacomoOracolo <gzano93@gmail.com>
2018-03-20 21:00:24 +0100

    ring stream pythin

--- camerascript.py
+++ camerascript.py
@@ -1,19 +1,25 @@
 import io
 import random
 import sys
-from time import sleep
 import picamera
+import random
 
 message = sys.stdin.readline()
+def motion_detected():
+    # Randomly return True (like a fake motion detection routine)
+    return random.randint(0, 10) == 0
 
 if message == "take pic\n":
-
-    camera = picamera.PiCamera()
-    camera.resolution = (640, 480)
-    camera.start_recording('my_video.h264')
-
-    camera.wait_recording(60)
-    camera.stop_recording()
-    print('frocio')
+       try:
+           while True:
+            camera.wait_recording(1)
+               if motion_detected():
+            # Keep recording for 10 seconds and only then write the
+            # stream to disk
+                  camera.wait_recording(10)
+                  stream.copy_to('motion.h264')
+        finally:
+          camera.stop_recording()
+    
 else:
     print('ghey')
