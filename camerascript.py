6ea0619df492e6f006e4f3db693429564c527f43
GiacomoOracolo <gzano93@gmail.com>
2018-03-20 18:57:31 +0100

    indentation error

--- camerascript.py
+++ camerascript.py
@@ -7,17 +7,13 @@ from picamera import PiCamera
 message = sys.stdin.readline()
 
 if message == "take pic\n":
-
-
-
-
-camera = picamera.PiCamera()
-stream = picamera.PiCameraCircularIO(camera, seconds=20)
-camera.start_recording(stream, format='h264')
+   camera = picamera.PiCamera()
+   stream = picamera.PiCameraCircularIO(camera, seconds=20)
+   camera.start_recording(stream, format='h264')
 try:
     while True:
-      
-            stream.copy_to('motion.h264')
+
+        stream.copy_to('motion.h264')
 finally:
     camera.stop_recording()
     print('pic taken')
