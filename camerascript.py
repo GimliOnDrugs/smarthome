50214b8eee11c59867baa6032b5c6589dd54752b
GiacomoOracolo <gzano93@gmail.com>
2018-04-21 12:39:46 +0200

    applied algorithm to video recording

--- camerascript.py
+++ camerascript.py
@@ -2,32 +2,35 @@ import io
 import random
 import sys
 import picamera
-
+import MotionDetector
 message = sys.stdin.readline()
 
+camera = picamera.PiCamera()
 
-def motion_detected():
-    # Randomly return True (like a fake motion detection routine)
-    return random.randint(0, 10) == 0
+
+motionDetector = MotionDetector(camera)
+
+
+
 
 
 if message == "start recording\n":
-    camera = picamera.PiCamera()
     stream = picamera.PiCameraCircularIO(camera, seconds=20)
     camera.start_recording(stream, format='h264')
     try:
 
         while True:
             camera.wait_recording(1)
-            if motion_detected():
+            motionDetector.detectMotion(stream)
+           # if motion_detected():
 
-                camera.wait_recording(10)
+                """ camera.wait_recording(10)
                 stream.copy_to('motion.h264')
                 print('video recorded')
                 camera.stop_recording()
                 if sys.stdin.readline() == "keep going\n":
-                    stream=picamera.PiCameraCircularIO(camera,seconds=20)
-                    camera.start_recording(stream, format='h264')
+                    stream = picamera.PiCameraCircularIO(camera, seconds=20)
+                    camera.start_recording(stream, format='h264') """
 
     finally:
         camera.stop_recording()
