da711d873026ac01aa158419bbd32c9f082a4d39
GiacomoOracolo <gzano93@gmail.com>
2018-04-22 14:25:36 +0200

    vf dc

--- pimotionrealtimeanalysis.py
+++ pimotionrealtimeanalysis.py
@@ -30,13 +30,13 @@ class DetectMotion(picamera.array.PiRGBAnalysis):
             # first frame
         #cv2.imwrite('frame.jpg',fgmask)
         frameDelta = cv2.absdiff(self.firstFrame, gray)
-        #name2 = 'debugdelta'+str(self.count)+'.jpg'
-        cv2.imwrite(name2,frameDelta)
+        name2 = 'debugdelta'+str(self.count)+'.jpg'
+        #cv2.imwrite(name2,frameDelta)
         thresh = cv2.threshold(frameDelta, 20, 255, cv2.THRESH_BINARY)[1]
 
         thresh = cv2.dilate(thresh, None, iterations=2)
-        #name = 'diff'+str(self.count)+'.jpg'
-        cv2.imwrite(name,thresh)
+        name = 'diff'+str(self.count)+'.jpg'
+        #cv2.imwrite(name,thresh)
         self.count += 1
 
         if cv2.countNonZero(thresh) > 20000:
