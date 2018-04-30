2a7c9ea63eed329f594d13f36ad51cc98f836582
GiacomoOracolo <gzano93@gmail.com>
2018-04-30 19:03:16 +0200

    minor

--- newscript.py
+++ newscript.py
@@ -16,7 +16,9 @@ timeFirstFrame = datetime.datetime.now().minute
 frame_count = 0
 detected = False
 face_cascade = cv2.CascadeClassifier('/home/pi/Documents/smarthome/smarthome/haarcascade_frontalface_default.xml')
-
+face_recognizer = cv2.face.LBPHFaceRecognizer_create()
+face_recognizer.read('/home/pi/Documents/smarthome/smarthome/trainingdata.xml')
+subjects = ["empty","Giacomo","Lucia","Obama"]
 
 def detect_motion(camera):
     global firstFrame
@@ -35,8 +37,13 @@ def detect_motion(camera):
     face_rects = face_cascade.detectMultiScale(current_frame, 1.3, 5)
     for (x, y, w, h) in face_rects:
         cv2.rectangle(current_frame, (x, y), (x+w, y+h), (0, 255, 0), 3)
+        cv2.rectangle(current_frame, (x, y), (x+w, y+h), (0, 255, 0), 3)
+        label,conf = face_recognizer.predict(current_frame[y:y+h,x:x+w])
+        cv2.putText(current_frame,subjects[label],(x,y), cv2.FONT_HERSHEY_PLAIN,1,(255,255,0),3)
+        cv2.putText(current_frame, str(conf),(x,y+h),cv2.FONT_HERSHEY_PLAIN,1,(255,255,0),3)
         cv2.imwrite('facedetected.jpg',current_frame)
 
+        
 with picamera.PiCamera() as camera:
         stream = picamera.PiCameraCircularIO(camera, seconds=5)
         camera.start_recording(stream, format='h264')
